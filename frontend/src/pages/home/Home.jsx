1b7f16f15cae49cdabb2517acf84b82788be14a1
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-04-27 12:29:16 +0530

    features yet to be added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,24 +4,17 @@ import MessageContainer from '../../components/messages/MessageContainer'
 import useConversation from '../../zustand/useConversation'
 
 export default function Home() {
-  const {selectedConversation}=useConversation()
+  // const {selectedConversation}=useConversation()
    
   return (
     
    
 
-    <div className='flex h-[550px]   rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
-      
-       
- 
+    <div className='flex h-[85%]  sm:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
  
 
       <Sidebar/>
- 
- 
-      
- 
- 
+
 
       <MessageContainer/>
  
